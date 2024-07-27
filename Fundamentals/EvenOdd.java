9567b81780b5e4c4d4596a7a6159007720cd8930
sreasgop <sreasgop@gmail.com>
2024-07-28 05:08:19 +0530

    EvenOdd.java and TimesTable.java added

---
+++ Fundamentals/EvenOdd.java
@@ -0,0 +1,20 @@
+// Write a program to ask the user to enter a number and tell whether entered number is even or odd. 
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int user_input = sc.nextInt();
+
+        if(user_input%2==0){
+            System.out.println(user_input+" is an even number.");
+        } else {
+            System.out.println(user_input+" is an odd number.");
+        }
+        
+    }
+}
