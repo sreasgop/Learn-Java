a1f597112c48ed3e966bc56555ba66618307924b
sreasgop <sreasgop@gmail.com>
2024-07-29 13:14:53 +0530

    Access Modifiers, Run-Time Polymorphism, Dynamic Memory Dispatch completed

--- Telusko Java Course/OOP_7_MethodOverriding.java
+++ Telusko Java Course/OOP_7_MethodOverriding.java
@@ -10,6 +10,12 @@ class B extends A{
     }
 }
 
+class C extends B{
+    public void show(){
+        System.out.println("In C Show");
+    }
+}
+
 // METHOD OVERRIDING 
 // If an object of a parent class is used to invoke the method, then the version in the parent class will be executed, but if an object of the subclass is used to invoke the method, then the version in the child class will be executed. In other words, it is the type of the object being referred to (not the type of the reference variable) that determines which version of an overridden method will be executed.
 
@@ -19,5 +25,16 @@ public class OOP_7_MethodOverriding {
         B obj = new B();
         obj.show();
 
+
+        // Dynamic Method Dispatch 
+
+        A obj2 = new A();   
+        obj2.show();        // Run-Time Polymorphism 
+
+        obj2 = new B();     // Reference of A and Object of B
+        obj2.show();
+
+        obj2 = new C();     // Reference of A and Object of C
+        obj2.show();
     }
 }
