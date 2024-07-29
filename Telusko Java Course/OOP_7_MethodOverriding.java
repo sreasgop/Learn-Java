53ac5c0a57a3fcfc20736ced75d7d8484a7c9dfe
sreasgop <sreasgop@gmail.com>
2024-07-29 16:48:05 +0530

    OOP_7_MethodOverriding.java updated: Proper documentation added

--- Telusko Java Course/OOP_7_MethodOverriding.java
+++ Telusko Java Course/OOP_7_MethodOverriding.java
@@ -1,3 +1,9 @@
+// METHOD OVERRIDING 
+// Method overriding is a feature in object-oriented programming that allows a subclass (or child class) to provide a specific implementation of a method that is already defined in its superclass (or parent class). This is particularly useful for runtime polymorphism, where the method that gets executed is determined by the object that calls it.
+
+// If an object of a parent class is used to invoke the method, then the version in the parent class will be executed, but if an object of the subclass is used to invoke the method, then the version in the child class will be executed. In other words, it is the type of the object being referred to (not the type of the reference variable) that determines which version of an overridden method will be executed.
+
+
 class A{
     public void show(){
         System.out.println("In A Show");
@@ -16,8 +22,7 @@ class C extends B{
     }
 }
 
-// METHOD OVERRIDING 
-// If an object of a parent class is used to invoke the method, then the version in the parent class will be executed, but if an object of the subclass is used to invoke the method, then the version in the child class will be executed. In other words, it is the type of the object being referred to (not the type of the reference variable) that determines which version of an overridden method will be executed.
+
 
 public class OOP_7_MethodOverriding {
     public static void main(String[] args) {
