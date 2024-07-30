7df6494e27913113f672517fcdeb0396add1813e
sreasgop <sreasgop@gmail.com>
2024-07-30 23:46:25 +0530

    OOP_14 added: Telusko's Abstraction Applicaiton implemented using interface

---
+++ Telusko Java Course/OOP_14.java
@@ -0,0 +1,57 @@
+interface Computer{
+    void speedOfWork();
+}
+
+class Desktop implements Computer{
+    public void speedOfWork(){
+        System.out.println("Fastest: Code, Compile, Execute.");
+    }
+}
+
+class Laptop implements Computer{
+    public void speedOfWork(){
+        System.out.println("Moderate: Code, Compile, Execute.");
+    }
+}
+
+class Mobile implements Computer{
+
+    public void speedOfWork() {
+        System.out.print("Slow: Code, Execute.");
+        System.out.println(" Slowest: Compile");
+        
+    }
+}
+
+
+class Programmer{
+    public void Code(Computer Device){
+        Device.speedOfWork();
+    }
+}
+
+
+public class OOP_14 {
+    public static void main(String[] args){
+
+        // Machines:
+        Computer machine1 = new Desktop(); 
+        Computer machine2 = new Laptop();
+        Computer machine3 = new Mobile(); 
+
+
+        // Developer: 
+        Programmer newEmployee = new Programmer();
+
+        System.out.print(machine1.getClass().getSimpleName()+" = ");
+        newEmployee.Code(machine1);
+        
+        System.out.print(machine2.getClass().getSimpleName()+" = ");
+        newEmployee.Code(machine2);
+
+        System.out.print(machine3.getClass().getSimpleName()+" = ");
+        newEmployee.Code(machine3);
+
+
+    }    
+}
