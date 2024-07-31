d4e869d4efb093bb004c779af3b8a50aa0460e21
sreasgop <sreasgop@gmail.com>
2024-08-01 00:05:04 +0530

    OOP_19_FunctionalInterface.java modified: Instantiated another objected after creating an anonymous inner class of reference type of interface A

--- Telusko Java Course/OOP_19_FunctionalInterface.java
+++ Telusko Java Course/OOP_19_FunctionalInterface.java
@@ -12,8 +12,17 @@ class B implements A{
 public class OOP_19_FunctionalInterface {
     public static void main(String[] args) {
         
+        // By creating an object of Class B with a Reference type of interface A
         A obj = new B();
         obj.show();
 
+        // By creating an anonymous inner class of reference type of interface A..
+        A obj2 = new A() {
+            public void show(){
+                System.out.println("In New Show");
+            }
+        };
+
+        obj2.show();
     }
 }
