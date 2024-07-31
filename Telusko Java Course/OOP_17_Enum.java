a0965282fbd89439be192a4b60b14c331e34156e
sreasgop <sreasgop@gmail.com>
2024-07-31 06:11:00 +0530

    OOP_17_Enum.java added

---
+++ Telusko Java Course/OOP_17_Enum.java
@@ -0,0 +1,34 @@
+enum Laptop{
+    Macbook(2000), XPS(2200), Surface(1500), ThinkPad(1800);
+
+    private int price; 
+
+    private Laptop(int price){
+        this.price = price;
+    }
+
+    public int getPrice(){
+        return this.price;
+    }
+
+    public void setPrice(int price){
+        this.price = price;
+    }
+
+}
+
+public class OOP_17_Enum {
+    public static void main(String[] args) {
+        
+        // Laptop machine = Laptop.Macbook;
+        // System.out.println(machine + " : " +machine.getPrice());
+
+        // machine.setPrice(20);
+        // System.out.println(machine.getPrice());
+
+        for(Laptop device: Laptop.values()){
+            System.out.println(device.ordinal() + " : " + device + " : " + device.getPrice());
+        }
+
+    }
+}
