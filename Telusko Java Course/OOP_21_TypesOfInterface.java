f7cb0fef55bb66637cf59a57c3b602c9a1e6360a
sreasgop <sreasgop@gmail.com>
2024-08-02 02:44:03 +0530

    OOP_21_TypesOfInterface.java added

---
+++ Telusko Java Course/OOP_21_TypesOfInterface.java
@@ -0,0 +1,10 @@
+// Types of Interfaces
+// 1. Normal Interface: An interface that has two or more methods are called normal interface. 
+// 2. Functionl Interface/SAM: An interface with only one method is called a functional interface, this type of interface is also called Single Abstract Method     
+// 3. Marker Interface: An interface with no methods, a blank interface is called a marker interface. 
+
+public class OOP_21_TypesOfInterface {
+    public static void main(String[] args) {
+            
+    }
+}
