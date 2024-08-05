5a84abd8cb501502d7055d276944314808bf10fe
sreasgop <sreasgop@gmail.com>
2024-08-05 22:24:21 +0530

    OOP_31_Collection.java added

---
+++ Telusko Java Course/OOP_31_Collection.java
@@ -0,0 +1,10 @@
+// Collection API: Concept
+// Collection: Interface
+// Collections: A class that implements the collection interface.
+
+
+public class OOP_31_Collection {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
