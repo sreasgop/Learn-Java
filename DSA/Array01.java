0d381087d72feef90f597277ab7e2707a615ef9c
sreasgop <sreasgop@gmail.com>
2024-08-09 00:20:22 +0530

    Array01.java modified: System.arraycopy() method used to copy an array to another.

--- DSA/Array01.java
+++ DSA/Array01.java
@@ -58,7 +58,7 @@ public class Array01{
 
         // CREATING ANOTHER ARRAY: 
         // Declarition, Instantiation and Initialization of Array:
-        int[] newarr = {2, 4, 6, 8, 10};
+        int[] newarr = {2, 4, 6, 8, 10, 12, 14, 15};
         
         // Printing the array using enhanced for loop:
         for (int i : newarr) {
@@ -71,6 +71,27 @@ public class Array01{
         // An anonymous array is nothing but an array that isn't assigned to a variable name. 
         // NOTE: We can't pass the same array to multiple methods or functions as we don't have a variable name attached to it. 
         Array01.max(new int[]{20, 10, 300, 99, -12, 90});
+        System.out.println();
+
+
+        // Creating a Copy Array using the .arraycopy() method: 
+        int[] copyarr = new int[5];
+
+        // .arraycopy() takes the array that we want to copy from as the 1st parameter. 
+        // .arraycopy() takes the index of the array where we want to copy from as the 2nd parameter.
+        // .arraycopy() takes the array that we want to copy to as it's 3rd paraemter.
+        // .arraycopy() takes the starting index where we want to copy to of the array where things are being coopied.
+        // .arraycopy() takes the ending index where we want to copy till in the array where things are being copied. 
+        System.arraycopy(newarr, 2, copyarr, 0, 5);
+        // In this example, the 1st parameter is newarr, which means we are copying from this array. 
+        // In the 2nd parameter we have 2, which means we are going to copy from the 2nd parameter of the newarr. 
+        // In the 3rd parameter we have copyarr, which means this is the new array where we are coppying the values to. 
+        // In the 4th and 5th parameter, we have mentioned the starting and ending index positions of the copyarr indicating from where to where we want to populate the array with copied elements from the newarr. 
+
+        for (int i : copyarr) {
+            System.out.print(i+" ");
+        }
+
 
     }
 }
