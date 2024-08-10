1e9e55c642be3865fbf0cc31ce5d2d214c586d63
sreasgop <sreasgop@gmail.com>
2024-08-11 04:57:09 +0530

    .\Array02_RandomElements.java added

---
+++ DSA/Array02_RandomElements.java
@@ -0,0 +1,27 @@
+// Write a program to create an array of random elements: 
+
+import java.util.Scanner;
+
+public class Array02_RandomElements {
+    public static void main(String[] args) {
+        
+        // Creating an Object of Scanner Class
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i=0; i< arr.length; i++){
+            arr[i] = ((int)(Math.random()*101));
+        }    
+
+        System.out.println("Array of random numbers: ");
+        for(int i: arr){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+    }
+}
