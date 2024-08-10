ac9c2bff68301c3af9a8b02fb8ec1ffe1e7ff79b
sreasgop <sreasgop@gmail.com>
2024-08-10 10:14:46 +0530

    OOP_44_StreamAPIPredicate.java modified: New documentation added

--- Telusko Java Course/OOP_44_StreamAPIPredicate.java
+++ Telusko Java Course/OOP_44_StreamAPIPredicate.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 import java.util.function.Predicate;
 
-public class OOP_44_StreamAPI2 {
+public class OOP_44_StreamAPIPredicate {
     public static void main(String[] args){
 
         List<Integer> listObj = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
