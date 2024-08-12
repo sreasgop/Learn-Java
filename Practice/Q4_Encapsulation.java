b1fe55433b606f684149d3f35de029d8a3b55b72
sreasgop <sreasgop@gmail.com>
2024-08-13 00:24:12 +0530

    Q4_Encapsulation.java modified: All functions implemented and main method written as well.

--- Practice/Q4_Encapsulation.java
+++ Practice/Q4_Encapsulation.java
@@ -1,26 +1,33 @@
 // Question: Create a class Account with private properties accountNumber, balance, and ownerName. Provide public getter and setter methods for these properties. Implement a method deposit() to add money to the account and withdraw() to subtract money from the account, ensuring the balance doesn't go negative.
 // Focus: Encapsulation and data hiding.
 
-import java.util.Random;
-
 class Account{
 
-    private static Random rand = new Random();
+    private static int accountSerialNo = 2024000;
 
     private final int accountNumber;
     private String userName;
     private float balance;
 
+
+
     // Constructors:
     public Account(){
-        this.accountNumber = rand.nextInt(9999);
+        this.accountNumber = accountSerialNo++;
     }
 
-    public Account(String userName, float balance) {
+    public Account(String userName){
         this();
         this.userName = userName;
-        this.balance = balance;
+
     }
+
+    public Account(String userName, float openingBalance) {
+        this();
+        this.userName = userName;
+        this.balance = openingBalance;
+    }
+
 
 
     // Setter Methods:
@@ -35,34 +42,39 @@ class Account{
         return acc.accountNumber;
     }
 
-
     public String getUserName() {
         return this.userName;
     }
-
 
     public int getAccountNumber() {
         return this.accountNumber;
     }
 
-
     public float getBalance() {
         return this.balance;
     }
 
+
+    
     // Functions:
     public int deposit(float money){
         this.balance+=money;
+        System.out.println("Transaction Summary: ");
+        System.out.println(money + " credited to account: " + this.accountNumber + "\nAvailable Balance: "+this.balance);
+        System.out.println();
         return 0;
     }
-
+    
     public int withdraw(float money){
+        System.out.println("Transaction Summary: ");
         if(money>this.balance){
-            System.out.print("Sufficient ammount unavailable to make withdrawal!\nAvailable Balance: ");
+            System.out.print("Insufficient ammount to make withdrawal!\nAvailable Balance: ");
             System.out.println(this.balance);
+            System.out.println();
             return 1;
         }
-        System.out.print("Transaction Successful: "+money + " withdrawn.\nAvailable Balance: ");
+        this.balance -= money;
+        System.out.print(money + " debited from account: " + this.accountNumber + ".\nAvailable Balance: ");
         System.out.println(this.balance);
         return 0;
     }
@@ -78,9 +90,19 @@ class Account{
 public class Q4_Encapsulation {
     public static void main(String[] args) {
         
-        Account accObj = new Account();
-        System.out.println(accObj);
+        Account accObj1 = new Account("Chandra Sreas Gop", 5500);
+        System.out.println(accObj1);
+
+        Account accObj2 = new Account("Soutrika Das"); 
+        System.out.println(accObj2);
+
+        Account accObj3 = new Account();
+        accObj3.setUserName("Sanjib Kumar Sah");
+        accObj3.deposit(500);
+        System.out.println(accObj3);
 
 
+        accObj1.deposit(500);
+        accObj1.withdraw(2000);
     }
 }
