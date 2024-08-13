aeae498d7cd85a36168242cd4d4fde398e56fc15
sreasgop <sreasgop@gmail.com>
2024-08-13 17:26:07 +0530

    Q_7Interface.java added

---
+++ Practice/Q7_Interface.java
@@ -0,0 +1,7 @@
+// Question: Define an interface Animal with methods eat() and sleep(). Implement this interface in classes Dog and Cat, providing specific implementations for each method. Create objects of Dog and Cat and call their methods.
+// Focus: Interface creation and implementation, achieving polymorphism through interfaces.
+
+interface Animal{
+    void eat();
+    void sleep();
+}
