09263fe967bc65b38fd1ea70b746acf9c6d7b262
sreasgop <sreasgop@gmail.com>
2024-08-18 04:13:12 +0530

    Array02_LargestElement.java added

---
+++ DSA/Array02_LargestElement.java
@@ -0,0 +1,21 @@
+// 
+
+public class Array02_LargestElement {
+    
+    public static void main(String[] args) {
+
+        System.out.println("Largest: " + largestElement(new int[]{10, 281, 28, 0, -12, 400, 0, 12, 3}));
+
+    }
+    
+    public static int largestElement(int [] arr){
+        int largest = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > largest){
+                largest = arr[i];
+            }
+        }
+        return largest;
+    }
+    
+}
