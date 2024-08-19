a33b3b3dc6ec3bda4a6dd77b800e252304f95a27
sreasgop <sreasgop@gmail.com>
2024-08-19 22:35:23 +0530

    .\Recursion04.java added: Comments added

--- DSA/Recursion04.java
+++ DSA/Recursion04.java
@@ -5,6 +5,7 @@ public class Recursion04 {
         printNum(10);
     }
 
+    // The function can print 10 to 1 using only one parameter as we have changed the base case condition to be equal to 0. Which means that if n equals to 0 the function will stop calling itself. 
     static void printNum(int n){
         if(n==0){
             return;
